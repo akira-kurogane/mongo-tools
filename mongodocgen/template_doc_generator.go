219cb05345eb10640ae3b610330d05987b0a35bf
Akira Kurogane <akira.kurogane@10gen.com>
2015-07-22 11:30:35 +1000

    Fixed for the previously forgotten array case.
    N.b. dummy handling of the "$macro" strings still in place.

--- mongodocgen/template_doc_generator.go
+++ mongodocgen/template_doc_generator.go
@@ -14,9 +14,17 @@ func appendMapAsBsonD (doc bson.D, m map[string]interface{}) bson.D {
 	for name, value := range m {
 		switch value.(type) {
 		case map[string]interface{}:
-			//fmt.Printf("%s has type %T\n", name, t)
 			newNestedDocElem := appendMapAsBsonD(bson.D{}, value.(map[string]interface{}))
 			doc = append(doc, bson.DocElem{name, newNestedDocElem})
+		case []interface{}:
+			newNestedArray := appendArrayAsBsonD(bson.D{}, value.([]interface{}))
+			doc = append(doc, bson.DocElem{name, newNestedArray})
+		case string:
+			if value.(string)[:1] == "$" {
+				fmt.Printf("Found %s\n", value)
+				value = "func goes here"
+			}
+			doc = append(doc, bson.DocElem{name, value})
 		default:
 			//fmt.Printf("%s has type %T\n", name, t)
 			doc = append(doc, bson.DocElem{name, value})
@@ -25,11 +33,27 @@ func appendMapAsBsonD (doc bson.D, m map[string]interface{}) bson.D {
 	return doc
 }
 
+func appendArrayAsBsonD (doc bson.D, a []interface{}) []interface{} {
+	docElemArray := make([]interface{}, len(a))
+	for ord, arrayVal := range a {
+		fmt.Printf("ord: %d, val: %#v\n", ord, arrayVal)
+		switch arrayVal.(type) {
+		case map[string]interface{}:
+			docElemArray[ord] = appendMapAsBsonD(bson.D{}, arrayVal.(map[string]interface{}))
+		case []interface{}:
+			docElemArray[ord] = appendArrayAsBsonD(bson.D{}, arrayVal.([]interface{}))
+		default:
+			docElemArray[ord] = arrayVal
+		}
+	}
+	return docElemArray
+}
 
 func NewTemplateDocumentGenerator(templateString string) (TemplateDocumentGenerator, error) {
 	tdg := TemplateDocumentGenerator{}
 	var templateAsMap map[string]interface{}
 	err := json.Unmarshal([]byte(templateString), &templateAsMap)
+	fmt.Printf("%#v\n", templateAsMap)
 	if err != nil {
 		return tdg, fmt.Errorf("Template string was invalid JSON. %s", err)
 	}
