81b0c1bb9decc2dd3c3884846c152355066204fd
Akira Kurogane <akira.kurogane@10gen.com>
2015-07-22 13:56:34 +1000

    stampOut funcs added

--- mongodocgen/template_doc_generator.go
+++ mongodocgen/template_doc_generator.go
@@ -21,8 +21,7 @@ func appendMapAsBsonD (doc bson.D, m map[string]interface{}) bson.D {
 			doc = append(doc, bson.DocElem{name, newNestedArray})
 		case string:
 			if value.(string)[:1] == "$" {
-				fmt.Printf("Found %s\n", value)
-				value = "func goes here"
+				value = "FUNC"
 			}
 			doc = append(doc, bson.DocElem{name, value})
 		default:
@@ -36,12 +35,16 @@ func appendMapAsBsonD (doc bson.D, m map[string]interface{}) bson.D {
 func appendArrayAsBsonD (doc bson.D, a []interface{}) []interface{} {
 	docElemArray := make([]interface{}, len(a))
 	for ord, arrayVal := range a {
-		fmt.Printf("ord: %d, val: %#v\n", ord, arrayVal)
 		switch arrayVal.(type) {
 		case map[string]interface{}:
 			docElemArray[ord] = appendMapAsBsonD(bson.D{}, arrayVal.(map[string]interface{}))
 		case []interface{}:
 			docElemArray[ord] = appendArrayAsBsonD(bson.D{}, arrayVal.([]interface{}))
+		case string:
+			if arrayVal.(string)[:1] == "$" {
+				arrayVal = "FUNC"
+			}
+			docElemArray[ord] = arrayVal
 		default:
 			docElemArray[ord] = arrayVal
 		}
@@ -61,12 +64,48 @@ func NewTemplateDocumentGenerator(templateString string) (TemplateDocumentGenera
 	return tdg, nil
 }
 
+func stampOutDocElem(plugDocElem bson.DocElem) (bson.DocElem, error) {
+	//fmt.Printf("Value Type %T elem: %v\n", plugDocElem.Value, plugDocElem)
+	switch plugDocElem.Value.(type) {
+	case bson.D:
+		y, _ := stampOut(plugDocElem.Value.(bson.D))
+		return bson.DocElem{plugDocElem.Name, y}, nil
+	case []interface{}:
+		a := make([]interface{}, len(plugDocElem.Value.([]interface{})))
+		for ord, arrayVal := range plugDocElem.Value.([]interface{}) {
+			x, _ := stampOutDocElem(bson.DocElem{"dummy_name", arrayVal})
+			a[ord] = x.Value
+		}
+		return bson.DocElem{plugDocElem.Name, a}, nil
+	case string:
+		if plugDocElem.Value.(string) == "FUNC" {
+			plugDocElem.Value = "FUNC -- 0000000"
+		}
+		return plugDocElem, nil
+	default:
+		return plugDocElem, nil
+	}
+}
+
+func stampOut(plugDoc bson.D) (bson.D, error) {
+	doc := make([]bson.DocElem, len(plugDoc))
+	for ord, plugDocElem := range plugDoc {
+		doc[ord], _ = stampOutDocElem(plugDocElem)
+	}
+	return doc, nil
+}
+
+func (tdg *TemplateDocumentGenerator) Next() (bson.D, error) {
+	return stampOut(tdg.Plug)
+}
+
 func (tdg *TemplateDocumentGenerator) StreamDocument(read chan bson.D) error {
 	go func() {
 		nextBatch := make([]bson.D, 1000)
 		for {
 			for i := range nextBatch {
-				nextBatch[i] = tdg.Plug
+				d, _ := stampOut(tdg.Plug)
+				nextBatch[i] = d
 			}
 			for i := range nextBatch {
 				read <- nextBatch[i]
